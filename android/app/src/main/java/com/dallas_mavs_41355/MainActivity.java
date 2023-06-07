b0cebd5198ff835fc1e99e59e2bf2f665867897b
Crowdbotics <team@crowdbotics.com>
2023-06-07 23:23:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dallas_mavs_41355/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dallas_mavs_41355;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dallas_mavs_41355";
+  }
+}
